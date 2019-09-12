c8bbe7cc41550f5e2b1635f986262bfdfcc599fe
kylebrain <kbrainard@nevada.unr.edu>
2019-09-11 20:20:33 -0700

    Created BFS, DFS, IDS

--- Makefile
+++ Makefile
@@ -1,9 +1,22 @@
 all: BFS DFS IDS
-BFS:
-	g++ RomaniaBFS.cpp -o BFS
 
-DFS:
-	g++ RomaniaDFS.cpp -o DFS
+BFS: bfs.o
+	g++ bfs.o -o BFS
 
-IDS:
-	g++ RomaniaIDS.cpp -o IDS
+bfs.o: Romania.cpp
+	g++ Romania.cpp -c -DIMPLENTING_BFS -o bfs.o
+
+DFS: dfs.o
+	g++ dfs.o -o DFS
+
+dfs.o: Romania.cpp
+	g++ Romania.cpp -c -DIMPLEMENTING_DFS -o dfs.o
+
+IDS: ids.o
+	g++ ids.o -o IDS
+
+ids.o: Romania.cpp
+	g++ Romania.cpp -c -DIMPLENTING_IDS -o ids.o
+
+clean:
+	rm BFS bfs.o DFS dfs.o IDS ids.o
--- Romania.cpp
+++ Romania.cpp
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <iostream>
+#include <list>
+#include <stack>
 
 //The number of vertices stored in our graph
 int vertexCount = 0;
@@ -11,9 +14,9 @@ FILE* output;
 AddEdge: Add an edge to the graph from vertex u to vertex v.
 
 Arugments:
-	int u: One of the vertices that form the edge
-	int v: The other vertice that forms the edge
-	int dist: The distance between u and v
+    int u: One of the vertices that form the edge
+    int v: The other vertice that forms the edge
+    int dist: The distance between u and v
 
 Adds an edge to the graph from vertex u to vertex v with the given distance dist.
 The graph is represented a a 2D array of integers, where each integer is a distance.
@@ -23,15 +26,15 @@ Returns void: No value returned
 */
 void AddEdge(int u, int v, int dist)
 {
-	graph[u][v] = dist;
-	graph[v][u] = dist;
+    graph[u][v] = dist;
+    graph[v][u] = dist;
 }
 
 /*
 ReadGraphFile: Read graph data in from a file to populate our graph structure.
 
 Arguments:
-	char* fname: The name of the file to read
+    char* fname: The name of the file to read
 
 Reads graph data in from a file to populate our graph structure. The format of the file
 defines a number of vertices at the top of the file, and edges for the graph for each
@@ -41,48 +44,48 @@ Returns bool: Returns true if the file was opened and read successfuly, and fals
 */
 bool ReadGraphFile(char* fname)
 {
-	FILE* file = fopen(fname, "r");
-	if (file)
-	{
-		rewind(file);
-		int a, b, d;
-		
-		fscanf(file, "%d", &vertexCount);
-		//fscanf_s(file, "%d", &vertexCount);
+    FILE* file = fopen(fname, "r");
+    if (file)
+    {
+        rewind(file);
+        int a, b, d;
+        
+        fscanf(file, "%d", &vertexCount);
+        //fscanf_s(file, "%d", &vertexCount);
 
-		graph = new int*[vertexCount];
-		for (int u = 0; u < vertexCount; u++)
-		{
-			graph[u] = new int[vertexCount];
+        graph = new int*[vertexCount];
+        for (int u = 0; u < vertexCount; u++)
+        {
+            graph[u] = new int[vertexCount];
 
-			for (int v = 0; v < vertexCount; v++)
-			{
-				graph[u][v] = 0;
-			}
-		}
+            for (int v = 0; v < vertexCount; v++)
+            {
+                graph[u][v] = 0;
+            }
+        }
 
-		while (fscanf(file, "%d %d %d", &a, &b, &d) != EOF)
-//		while (fscanf_s(file, "%d %d %d", &a, &b, &d) != EOF)
-		{
-			AddEdge(a, b, d);
-		}
+        while (fscanf(file, "%d %d %d", &a, &b, &d) != EOF)
+//        while (fscanf_s(file, "%d %d %d", &a, &b, &d) != EOF)
+        {
+            AddEdge(a, b, d);
+        }
 
-		fclose(file);
+        fclose(file);
 
-		return true;
-	}
-	else
-	{
-		printf("Failed to read file...\n");
-		return false;
-	}
+        return true;
+    }
+    else
+    {
+        printf("Failed to read file...\n");
+        return false;
+    }
 }
 
 /*
 LogVertex: Prints the current vertex traveled to and saves the output to a file
 
 Arguments:
-	int vert: The vertex that you are currently visiting
+    int vert: The vertex that you are currently visiting
 
 In order for us to grade you, you must print the vertex you are travelling to in your algorithm.
 This function makes it easier for you to print the proper data. It also saves the data to a file.
@@ -91,15 +94,15 @@ Returns void: No values returned
 */
 void LogVertex(int vert)
 {
-	printf("%d\n", vert);
-	fprintf(output, "%d\n", vert);
+    printf("%d\n", vert);
+    fprintf(output, "%d\n", vert);
 }
 
 /*
 SetartLogging: Opens the logging file for later writing
 
 Arguments:
-	None
+    None
 
 Pretty much just a wrapper for fopen so that we make sure you are writing your logging data to the right file.
 
@@ -107,14 +110,14 @@ Returns void: No values returned
 */
 void StartLogging()
 {
-	output = fopen("log.txt", "w");
+    output = fopen("log.txt", "w");
 }
 
 /*
 EndLogging: Closes the logging file properly
 
 Arguments:
-	None
+    None
 
 Pretty much just a wrapper for fclose so that we make sure you close the right file.
 
@@ -122,36 +125,169 @@ Returns void: No values returned
 */
 void EndLogging()
 {
-	fclose(output);
+    fclose(output);
 }
+
+bool bfs(int start, int end);
+bool dfs(int start, int end);
+bool ids(int start, int end);
+bool recursive_dfs(int start, int end, int limit);
 
 int main(int argc, char** argv)
 {
-	//Setup
-	//-----------------------------------
-	if (argc == 4)
-	{
-		if (!ReadGraphFile(argv[1]))
-			return 2;
-	}
-	else
-	{
-		printf("No file argument given...\n");
-		return 1;
-	}
-	StartLogging();
-	//-------------------------------------
+    //Setup
+    //-----------------------------------
+    if (argc == 4)
+    {
+        if (!ReadGraphFile(argv[1]))
+            return 2;
+    }
+    else
+    {
+        printf("No file argument given...\n");
+        return 1;
+    }
+    StartLogging();
+    //-------------------------------------
+
+    int start = std::stoi(argv[2]);
+    int end = std::stoi(argv[3]);
+
+    //Your code
+    #if defined(IMPLENTING_BFS)
+    bfs(start, end);
+    #elif defined(IMPLEMENTING_DFS)
+    dfs(start, end);
+    #elif defined(IMPLENTING_IDS)
+    ids(start, end);
+    #endif
 
 
+    //Cleanup
+    //-------------------------------------
+    EndLogging();
+    //-------------------------------------
 
-	//Your code
+    return 0;
+}
 
+bool bfs(int start, int end)
+{
+    int visited[vertexCount];
+    for(int i = 0; i < vertexCount; i++)
+    {
+        visited[i] = false;
+    }
 
-	//Cleanup
-	//-------------------------------------
-	EndLogging();
-	getchar();
-	//-------------------------------------
+    std::list<int> queue;
 
-	return 0;
+    visited[start] = true;
+    queue.push_back(start);
+
+    while(!queue.empty())
+    {
+        int currentNode = queue.front();
+        queue.pop_front();
+
+        LogVertex(currentNode);
+
+        if(currentNode == end)
+        {
+            return true;
+        }
+
+        for(int i = 0; i < vertexCount; i++)
+        {
+            int distance = graph[currentNode][i];
+            if(distance != 0 && !visited[i])
+            {
+
+                visited[i] = true;
+                queue.push_back(i);
+            }
+        }
+    }
+
+    return false;
+}
+
+bool dfs(int start, int end)
+{
+    bool visited[vertexCount];
+    for(int i = 0; i < vertexCount; i++)
+    {
+        visited[i] = false;
+    }
+
+    std::stack<int> stack;
+
+    visited[start] = true;
+    stack.push(start);
+
+    while(!stack.empty())
+    {
+        int currentNode = stack.top();
+        stack.pop();
+
+        LogVertex(currentNode);
+
+        if(currentNode == end)
+        {
+            return true;
+        }
+
+        for(int i = 0; i < vertexCount; i++)
+        {
+            int distance = graph[currentNode][i];
+            if(distance != 0 && !visited[i])
+            {
+                visited[i] = true;
+                stack.push(i);
+            }
+        }
+    }
+
+    return false;
+}
+
+bool ids(int start, int end)
+{
+    for(int i = 0; i < vertexCount; i++)
+    {
+        if(recursive_dfs(start, end, i))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool recursive_dfs(int start, int end, int limit)
+{
+    LogVertex(start);
+
+    if(start == end)
+    {
+        return true;
+    }
+
+    if(limit <= 0)
+    {
+        return false;
+    }
+
+    for(int i = 0; i < vertexCount; i++)
+    {
+        int distance = graph[start][i];
+        if(distance != 0)
+        {
+            if(recursive_dfs(i, end, limit - 1))
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
